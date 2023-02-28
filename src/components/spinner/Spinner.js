edcb052af2aee9519e84b95e5b38db243d00b434
Agnes <almoha@gmail.com>
2023-02-28 19:00:43 +0100

    spinner

---
+++ src/components/spinner/Spinner.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Spinner.css";
+
+
+
+const Spinner = () => {
+  return (
+   
+      <div id="loader-wrapper">
+        <div id="loader"></div>
+
+        <div className="loader-section section-left"></div>
+        <div className="loader-section section-right"></div>
+      </div>
+   
+  );
+};
+
+export default Spinner;
