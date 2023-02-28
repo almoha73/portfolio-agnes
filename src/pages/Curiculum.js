5606316e0d8de0b09cc13f075d77f995672edd99
Agnes <almoha@gmail.com>
2023-02-28 14:43:49 +0100

    changement de la photo

--- src/pages/Curiculum.js
+++ src/pages/Curiculum.js
@@ -7,7 +7,7 @@ const Curiculum = () => {
 			<Navbar />
 			<div className="flex flex-col  xl:p-24 items-center justify-center w-full  xl:w-2/3 h-screen gap-8 text-[#802836]">
 				<a
-					href="https://firebasestorage.googleapis.com/v0/b/portfolio-e5e20.appspot.com/o/CV%2FCV.pdf?alt=media&token=e8511cda-cf06-4bdf-bc46-0892b6040914"
+					href="https://firebasestorage.googleapis.com/v0/b/perso-6e317.appspot.com/o/CV_Beaumatin_Agnes-Dev-Front.pdf?alt=media&token=6e69f5fc-898d-4349-80fe-9482222f5638"
 					className="text-4xl   hover:text-white hover:bg-[#802836] border shadow-lg px-4 py-2 rounded-lg"
 					target="_blank"
 					rel="noreferrer"
