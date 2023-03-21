5ec921fc369b180eae672faf16abac4a7df316a9
Agnes <almoha@gmail.com>
2023-03-21 18:40:35 +0100

    animation sur les cartes du portfolio

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,66 +1,86 @@
-import React from "react";
+import React, {useState, useRef, useEffect} from "react";
 
 const Card = ({ href, text, page, code, name, npm }) => {
-	return (
-		<>
-			<div className="flex flex-col xl:flex-row border p-4 w-11/12 text-black h-auto rounded shadow-lg">
-				{href && (
-					<div className="flex justify-center">
-						<img
-							src={href}
-							alt="project screenshot"
-							className="xl:max-w-[700px]"
-						/>
-					</div>
-				)}
+  const [show, setShow] = useState(false);
+  const ref = useRef(null);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const entry = entries[0];
+        if (entry.isIntersecting) {
+          setShow(true);
+          observer.unobserve(entry.target);
+        }
+      },
+      { threshold: 0.2 }
+    );
+    observer.observe(ref.current);
+  }, []);
+  return (
+    <>
+      <div
+        className={`flex flex-col xl:flex-row border p-4 w-11/12 text-black h-auto rounded shadow-lg ${
+          show ? "animate-fade-in" : ""
+        }`}
+        ref={ref}
+      >
+        {href && (
+          <div className="flex justify-center">
+            <img
+              src={href}
+              alt="project screenshot"
+              className="xl:max-w-[700px]"
+            />
+          </div>
+        )}
 
-				<div className="py-2  xl:px-6">
-					<p className="text-xl text-justify">{text}</p>
-					<br />
-					{href ? (
-						<p className="text-xl">
-							Lien github pages:
-							<a
-								href={page}
-								className="text-blue-500 hover:text-[#802836]"
-								target="_blank"
-								rel="noreferrer"
-							>
-								{" "}
-								{name}
-							</a>
-						</p>
-					) : (
-						<p className="text-xl">
-							Lien du package sur npm:
-							<a
-								href={npm}
-								className="text-blue-500 hover:text-[#802836]"
-								target="_blank"
-								rel="noreferrer"
-							>
-								{" "}
-								{name}
-							</a>
-						</p>
-					)}
+        <div className="py-2  xl:px-6">
+          <p className="text-xl text-justify">{text}</p>
+          <br />
+          {href ? (
+            <p className="text-xl">
+              Lien github pages:
+              <a
+                href={page}
+                className="text-blue-500 hover:text-[#802836]"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {" "}
+                {name}
+              </a>
+            </p>
+          ) : (
+            <p className="text-xl">
+              Lien du package sur npm:
+              <a
+                href={npm}
+                className="text-blue-500 hover:text-[#802836]"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {" "}
+                {name}
+              </a>
+            </p>
+          )}
 
-					<p className="text-xl">
-						Lien vers le code sur github:{" "}
-						<a
-							href={code}
-							className="text-blue-500 hover:text-[#802836]"
-							target="_blank"
-							rel="noreferrer"
-						>
-							{" "}
-							Code {name}
-						</a>
-					</p>
-				</div>
-			</div>
-		</>
-	);
+          <p className="text-xl">
+            Lien vers le code sur github:{" "}
+            <a
+              href={code}
+              className="text-blue-500 hover:text-[#802836]"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              Code {name}
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Card;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -5,62 +5,61 @@ import Card from "../components/Card";
 import { uuidv4 } from "@firebase/util";
 
 const Portfolio = () => {
-	const [screenshots, setScreenshots] = useState([]);
-	useEffect(() => {
-		const loadData = async () => {
-			const array = [];
-			const d = await getScreenshots();
-			await d.forEach((query) =>
-				array.push({ key: query.id, screenshots: query.data() })
-				
-			);
-			array.sort((a,b)=> a.screenshots.order - b.screenshots.order)
-			setScreenshots(array);
-		};
-		loadData();
-	}, [screenshots.length]);
+  const [screenshots, setScreenshots] = useState([]);
+  useEffect(() => {
+    const loadData = async () => {
+      const array = [];
+      const d = await getScreenshots();
+      await d.forEach((query) =>
+        array.push({ key: query.id, screenshots: query.data() })
+      );
+      array.sort((a, b) => a.screenshots.order - b.screenshots.order);
+      setScreenshots(array);
+    };
+    loadData();
+  }, [screenshots.length]);
 
-	console.log(screenshots);
+  console.log(screenshots);
 
-	return (
-		<div className=" flex flex-col items-center w-full h-auto bg-[#F1EBED]  ">
-			<Navbar />
-			<main className="w-full h-auto mt-16 sm:mt-24 mb-16 text-black">
-				<p className="w-11/12 mx-auto mb-4">
-					Lien du code sur github de ce portfolio:{" "}
-					<a
-						href="https://github.com/almoha73/portfolio-agnes"
-						className="text-xl text-blue-500"
-					>
-						Cliquez ici
-					</a>
-				</p>
-				<p className=" w-11/12 mx-auto mb-4">
-					Lien vers mon github:{" "}
-					<a
-						href="https://github.com/almoha73"
-						className="text-xl text-blue-500"
-					>
-						Cliquez ici
-					</a>
-				</p>
-				<div className=" flex flex-col gap-5 items-center justify-center">
-					{screenshots &&
-						screenshots.map((elt) => (
-							<Card
-								href={elt.screenshots.href}
-								npm={elt.screenshots.npm}
-								text={elt.screenshots.text}
-								code={elt.screenshots.code}
-								page={elt.screenshots.page}
-								name={elt.screenshots.name}
-								key={uuidv4()}
-							/>
-						))}
-				</div>
-			</main>
-		</div>
-	);
+  return (
+    <div className=" flex flex-col items-center w-full h-auto bg-[#F1EBED]  ">
+      <Navbar />
+      <main className="w-full h-auto mt-16 sm:mt-24 mb-16 text-black">
+        <p className="w-11/12 mx-auto mb-4">
+          Lien du code sur github de ce portfolio:{" "}
+          <a
+            href="https://github.com/almoha73/portfolio-agnes"
+            className="text-xl text-blue-500"
+          >
+            Cliquez ici
+          </a>
+        </p>
+        <p className=" w-11/12 mx-auto mb-4">
+          Lien vers mon github:{" "}
+          <a
+            href="https://github.com/almoha73"
+            className="text-xl text-blue-500"
+          >
+            Cliquez ici
+          </a>
+        </p>
+        <div className=" flex flex-col gap-5 items-center justify-center">
+          {screenshots &&
+            screenshots.map((elt) => (
+              <Card
+                href={elt.screenshots.href}
+                npm={elt.screenshots.npm}
+                text={elt.screenshots.text}
+                code={elt.screenshots.code}
+                page={elt.screenshots.page}
+                name={elt.screenshots.name}
+                key={uuidv4()}
+              />
+            ))}
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Portfolio;
